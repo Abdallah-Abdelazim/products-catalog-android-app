ae2fb8c769427eb1c608f0e60055228253dcb896
Abdallah Abdelazim <abdallah.abdelazim@hotmail.com>
2023-04-06 18:08:13 +0200

    Rename fragments

--- app/src/main/java/com/abdallah_abdelazim/products_catalog/ui/products_details/ProductDetailsFragment.kt
+++ app/src/main/java/com/abdallah_abdelazim/products_catalog/ui/products_details/ProductDetailsFragment.kt
@@ -1,4 +1,4 @@
-package com.abdallah_abdelazim.products_catalog.ui
+package com.abdallah_abdelazim.products_catalog.ui.products_details
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,15 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.abdallah_abdelazim.products_catalog.R
-import com.abdallah_abdelazim.products_catalog.databinding.FragmentFirstBinding
+import com.abdallah_abdelazim.products_catalog.databinding.FragmentProductDetailsBinding
 
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class ProductDetailsFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentProductDetailsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +24,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentProductDetailsBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -33,8 +32,10 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(
+                ProductDetailsFragmentDirections.actionProductDetailsFragmentToProductsListFragment()
+            )
         }
     }
 
--- app/src/main/java/com/abdallah_abdelazim/products_catalog/ui/products_list/ProductsListFragment.kt
+++ app/src/main/java/com/abdallah_abdelazim/products_catalog/ui/products_list/ProductsListFragment.kt
@@ -1,4 +1,4 @@
-package com.abdallah_abdelazim.products_catalog.ui
+package com.abdallah_abdelazim.products_catalog.ui.products_list
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,15 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.abdallah_abdelazim.products_catalog.R
-import com.abdallah_abdelazim.products_catalog.databinding.FragmentSecondBinding
+import com.abdallah_abdelazim.products_catalog.databinding.FragmentProductsListBinding
 
 /**
- * A simple [Fragment] subclass as the second destination in the navigation.
+ * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class ProductsListFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentProductsListBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +24,7 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentProductsListBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -33,8 +32,10 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(
+                ProductsListFragmentDirections.actionProductsListFragmentToProductDetailsFragment()
+            )
         }
     }
 
