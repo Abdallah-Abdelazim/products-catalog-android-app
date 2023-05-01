1f867c3144c315a19b38b85007fb7ab218f60be4
Abdallah Abdelazim <abdallah.abdelazim@hotmail.com>
2023-05-01 23:17:16 +0300

    Bump AGP version along with some dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,17 +32,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         viewBinding = true
         dataBinding = true
     }
-    packagingOptions {
+    packaging {
         resources {
             excludes.add("/META-INF/{AL2.0,LGPL2.1}")
         }
